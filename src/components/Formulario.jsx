a1be304ab11ca080c1df35242a0537fbca60fc79
maximiliano <maxil5gomez@gmail.com>
2022-06-10 23:19:04 -0300

    leyendo inputs y agregandolos en el state

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Enviando Formulario...')
+  }
+
   return (
-    <div className='md:w-1/2 lg:w-2/5 '>
+    <div className='md:w-1/2 lg:w-2/5 mx-5 '>
 
       <h2 className='font-black text-center text-3xl'>
         Seguimiento Pacientes
@@ -13,7 +24,7 @@ const Formulario = () => {
           Administralos
         </span>
       </p>
-      <form className='bg-white shadow-md rounded-lg py-10 mb-10 px-5 border-2'>
+      <form  onSubmit={handleSubmit} className='bg-white shadow-md rounded-lg py-10 mb-10 px-5 border-2'>
 
         <div className='mb-5'>
           <label htmlFor='paciente' className='block text-gray-700 uppercase font-bold'>Apellido y Nombre Paciente</label>
@@ -22,6 +33,8 @@ const Formulario = () => {
             type="text"
             placeholder='Apellido y Nombre del Paciente' 
             className='border-2 w-full mt-2 p-2 placeholder-gray-400 rounded-md'
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
@@ -32,6 +45,8 @@ const Formulario = () => {
             type="email"
             placeholder='Email Paciente' 
             className='border-2 w-full mt-2 p-2 placeholder-gray-400 rounded-md'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -41,13 +56,26 @@ const Formulario = () => {
             id='positivo'
             type="date"
             className='border-2 w-full mt-2 p-2 placeholder-gray-400 rounded-md'
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
 
         <div className='mb-5'>
           <label htmlFor='Sintomas' className='block text-gray-700 uppercase font-bold'>Sintomas</label>
-          <textarea className='border-2 w-full mt-2 p-2 rounded-md text-gray-500' id='sintomas' placeholder='Describe los sintomas'/>
-         <input className='bg-amber-700 w-full mt-2 p-3 text-white rounded-md hover:bg-indigo-700 transition-all cursor-pointer' type="submit" value='Agregar paciente' />
+          <textarea 
+            className='border-2 w-full mt-2 p-2 rounded-md text-gray-500' 
+            id='sintomas' 
+            placeholder='Describe los sintomas'
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}  
+          />
+         <input 
+          className='bg-amber-700 w-full mt-2 p-3 text-white rounded-md hover:bg-indigo-700 transition-all cursor-pointer' 
+          type="submit" 
+          value='Agregar paciente' 
+          
+          />
         </div>
 
         
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Paciente = () => {
   return (
-    <div className='m-3 bg-white shadow px-5 py-10 rounded-lg border-2'> 
+    <div className='mx-5 my-10 bg-white shadow px-5 py-10 rounded-lg border-2'> 
         <p className='font-bold mb-3 uppercase text-gray-700'>
             Nombre: {""}
             <span className='font-normal normal-case'>Maxi</span>
